f19df3bb0bd2325dee1a4725c4a97d7a466f58df
FloWers0930 <157562133+FloWers0930@users.noreply.github.com>
2026-03-28 14:01:17 +0800

    Update App.jsx

--- Flores_client/src/App.jsx
+++ Flores_client/src/App.jsx
@@ -1,21 +1,27 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Layout from './Components/Layout'
-import HomePage from './pages/HomePage'
-import ArticlePage from './pages/ArticlePage'
-import AboutPage from './pages/AboutPage'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages & Layout
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage";
+import ArticlePage from "./pages/ArticlePage";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/" element={<Layout />}>
+   
           <Route index element={<HomePage />} />
-          <Route path="articles" element={<ArticlePage />} />
+
           <Route path="about" element={<AboutPage />} />
+
+          <Route path="articles" element={<ArticlePage />} />
         </Route>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
