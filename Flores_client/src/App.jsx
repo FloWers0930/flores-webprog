ffbc126bfcec19b9e71f493aa74fab39835243ab
FloWers0930 <157562133+FloWers0930@users.noreply.github.com>
2026-03-28 14:03:52 +0800

    Update App.jsx

--- Flores_client/src/App.jsx
+++ Flores_client/src/App.jsx
@@ -1,26 +1,39 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-// Pages & Layout
-import Layout from "./components/Layout";
-import HomePage from "./pages/HomePage";
-import ArticlePage from "./pages/ArticlePage";
-import AboutPage from "./pages/AboutPage";
+// HomePage Structure
+import Layout from './components/Layout';
+import ArticlePage from './pages/ArticlePage';
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+
+const routes = [
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <HomePage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'articles',
+        element: <ArticlePage />,
+      },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routes);
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-
-        <Route path="/" element={<Layout />}>
-   
-          <Route index element={<HomePage />} />
-
-          <Route path="about" element={<AboutPage />} />
-
-          <Route path="articles" element={<ArticlePage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
